b4bbe73cf4a5cc1c7051f1a68bf42b053f1c464e
wabrocker <billbrocker@gmail.com>
2024-01-17 10:06:25 -0500

    made asset canister mods

--- src/nft_assets_frontend/src/index.js
+++ src/nft_assets_frontend/src/index.js
@@ -1,19 +1,1 @@
-import { nft_assets_backend } from "../../declarations/nft_assets_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await nft_assets_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
-});
